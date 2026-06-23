decf612064961452b0840b9c05bb87d238df74bf
Exwhyzed-del <exwhyzed596@gmail.com>
2026-06-23 10:36:59 +0530

    Revert "Set car wallpaper as default for new users"
    This reverts commit 6d2f73ee590233d0e474982d9dcb71ac836ca3e6.

--- src/store/useOSStore.ts
+++ src/store/useOSStore.ts
@@ -50,7 +50,7 @@ const initialWindows: WindowState[] = [
 ];
 
 let zIndexCounter = 20;
-const defaultWallpaper = "/wallpapers/car.jpg";
+const defaultWallpaper = "";
 
 export const useOSStore = create<OSState>()(
   persist(
